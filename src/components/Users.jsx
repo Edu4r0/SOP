3b5e79fccadde07144ef81c64ccffec61826a497
Edu4r0 <116533491+Edu4r0@users.noreply.github.com>
2023-08-11 16:47:55 -0500

    Changes fileter table

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -10,21 +10,25 @@ function Users() {
       .then((data) => setDataUser(data));
   }
 
-  console.log(DataUser);
+  const FilterUser = DataUser.map((data)=>
+    console.log(data)  
+  )
+    
+
   return (
-    <div className="bg-slate-900 w-full py-10">
+ 
+    <div  className="bg-slate-900 w-full py-10">
       <div className="grid place-items-center my-5">
         <form
           onSubmit={(e) => {
-            e.preventDefault();
-            User();
+            e.preventDefault()
           }}
         >
           <input
             className="bg-gray-800 border-2 h-7 text-white text-sm  py-1 px-1 rounded-md focus:outline-none  border-gray-600 mx-5"
             type="text"
           />
-          <button className="bg-blue-800 px-2 hover:bg-blue-600 h-7 text-white rounded-md">
+          <button onClick={FilterUser} className="bg-blue-800 px-2 hover:bg-blue-600 h-7 text-white rounded-md">
             Buscar
           </button>
         </form>
@@ -61,8 +65,10 @@ function Users() {
           </tbody>
         </table>
       </div>
+      <button onClick={User}>Click</button>
     </div>
   );
 }
+
 
 export default Users;
