49588d523f66fa472ee54be1f240be510975f88c
Edu4r0 <116533491+Edu4r0@users.noreply.github.com>
2023-08-18 16:59:53 -0500

    Changes

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
 function Image() {
+  const [file, setfile] = useState(false)
+
+  function handleClick() {
+    const inputfile = document.getElementById('inputfile')
+    inputfile.addEventListener('click',setfile(!file))
+  }
+
   return (
     <main className="bg-slate-900 w-full">
       <div className="flex flex-col items-center justify-center py-10">
@@ -7,7 +16,8 @@ function Image() {
         </h2>
         <div className=" bg-slate-800 h-60 w-2/3 my-5">
           <div className="flex justify-center items-center h-full border-dashed border-blue-800 border-8 w-full">
-            <input className="hidden" type="file" name="unpload" id="" />
+            <input className="hidden" type="file" name="unpload" id="inputfile" />
+            <span onClick={handleClick} className="underline text-white hover:text-blue-600 cursor-pointer">Unpload Image</span>
           </div>
         </div>
       </div>
