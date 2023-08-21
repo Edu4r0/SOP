982f610b2e754425e9f27912edbb78bcd274dde0
Edu4r0 <eduardobarboza1520@gmail.com>
2023-08-20 21:54:25 -0500

    img preview

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 
 function Image() {
-  const [file, setfile] = useState(false)
+  const [file, setfile] = useState('')
 
   function handleClick() {
     const inputfile = document.getElementById('inputfile')
     inputfile.click()
+  }
+  function handleChange({target : {files}}){
+    setfile(URL.createObjectURL(files[0]))
+
   }
 
   return (
@@ -16,10 +20,11 @@ function Image() {
         </h2>
         <div className=" bg-slate-800 h-60 w-2/3 my-5">
           <div className="flex justify-center items-center h-full border-dashed border-blue-800 border-8 w-full">
-            <input className="hidden" type="file" name="unpload" id="unpload" />
-            <a onClick={(e)=> {handleClick(e)}} className="underline" href="#">Unpload Image</a>
-            <input className="hidden" type="file" name="unpload" id="inputfile" />
+            <input onChange={(e) => {handleChange(e)}} className="hidden" type="file" accept="image/png, image/jpeg" name="unpload" id="inputfile" />
             <span onClick={handleClick} className="underline text-white hover:text-blue-600 cursor-pointer">Unpload Image</span>
+            <div>
+              <img src={file} alt="" />
+            </div>
           </div>
         </div>
       </div>
