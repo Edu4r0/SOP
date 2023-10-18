9bd3306b8d1eac3c140e4fa11f3c4a24d134fce5
Edu4r0 <116533491+Edu4r0@users.noreply.github.com>
2023-10-18 16:54:34 -0500

    Fix style button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -92,7 +92,7 @@ async  function handleSubmit(e) {
               </div>
               <button
                 type="submit"
-                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-800 "
+                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-800 "
               >
                 Iniciar sesion
               </button>
