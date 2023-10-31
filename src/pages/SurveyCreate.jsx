e7babe78755a1f46a504443d5ef287e137f35486
Edu4r0 <116533491+Edu4r0@users.noreply.github.com>
2023-10-31 15:38:06 -0500

    Update Url

--- src/pages/SurveyCreate.jsx
+++ src/pages/SurveyCreate.jsx
@@ -10,7 +10,7 @@ function SurveyCreate() {
   async function postData(questionData) {
     try {
       toast.loading("Cargando..");
-      const response = await fetch("https://api-sop.vercel.app/api/v1/survey", {
+      const response = await fetch("https://api-sop.vercel.app/api/v1/create/survey", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
