83cd0b14b943828a1acb0f713d9bedc5c7f2694f
Edu4r0 <116533491+Edu4r0@users.noreply.github.com>
2023-11-07 15:35:04 -0500

    Fix bug sidebar cut

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -4,11 +4,11 @@ function Users() {
   
 
   return (
-    <main className="bg-gray-50  dark:bg-gray-900 w-full px-5 py-5">
+    <main className="bg-gray-50 dark:bg-gray-900 w-full px-5 py-5">
       <h2 className="font-bold dark:text-white text-2xl">Usuarios</h2>
       <hr className="my-3 border border-slate-300 dark:border-slate-600 rounded-lg mx-auto" />
       
-      <div className="px-11 py-10 h-screen">
+      <div className="px-11 py-10 ">
          <UsersTable/> 
       </div>
     </main>
